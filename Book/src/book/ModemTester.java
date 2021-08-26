f3c568d46381585ec5c0b5328e149002cc07cac1
JosephMcguinn <JosephMcguinn@localhost>
2021-08-25 19:47:50 -0500

    Object Oriented Programming example from book.

---
+++ Book/src/book/ModemTester.java
@@ -0,0 +1,19 @@
+package book;
+
+public class ModemTester {
+
+	public static void main(String[] args) {
+		CableModem surfBoard = new CableModem();
+		DslModem gateway = new DslModem();
+		surfBoard.speed = 500000;
+		gateway.speed = 400000;
+		System.out.println("Trying the cable modem:");
+		surfBoard.displaySpeed();
+		surfBoard.connect();
+		System.out.println("Trying the DSL Modem:");
+		gateway.displaySpeed();
+		gateway.connect();
+		
+	}
+
+}
