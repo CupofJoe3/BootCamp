302415c865798142e1c54be07bcabd9b131f470c
JosephMcguinn <JosephMcguinn@localhost>
2021-08-26 09:55:07 -0500

    Picture with characters.

---
+++ Book/src/book/ROFLCopter.java
@@ -0,0 +1,59 @@
+package book;
+
+public class ROFLCopter {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		while(true) {
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("          LOL:ROFL:ROFL:                        ");
+		System.out.println("           A             ");
+		System.out.println("       /---------                       ");
+		System.out.println("LOL=====       []\\                        ");
+		System.out.println(" L    \\           \\         ");
+		System.out.println("       \\___________]                 ");
+		System.out.println("           I       I      ");
+		System.out.println("          -----------/              ");
+		System.out.println("                        ");
+		System.out.println("                        ");
+		System.out.println("Copter go vroom!!!");
+		
+		Thread.sleep(100);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("ROFL:ROFL:LOL:                              ");
+		System.out.println("           A             ");
+		System.out.println(" L     /---------                       ");
+		System.out.println("LOL=====       []\\                        ");
+		System.out.println("      \\           \\         ");
+		System.out.println("       \\___________]                 ");
+		System.out.println("           I       I      ");
+		System.out.println("          -----------/              ");
+		System.out.println("                        ");
+		System.out.println("                        ");
+		System.out.println("Copter go vroom!!!");
+		
+		Thread.sleep(100);
+
+		}
+		
+		
+		
+	}
+
+}
